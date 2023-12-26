c9a36fde0fd0d9cfc06cc2f1f3f4b1bcf8a7e0bf
Valerie Phoenix <valerie@techbychoice.org>
2023-12-25 19:02:51 -0800

    tbc-71 dynamic slack messages
    - I added a new component that breaks down messages from Slack 
    - Updated the member dashboard to see the Slack message (#32)

---
+++ src/compoents/SlackMessage.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const SlackMessage = ({ elements }) => {
+    const renderElement = element => {
+        switch (element.type) {
+            case 'text':
+                return <span style={element.style?.bold ? { fontWeight: 'bold' } : {}}>{element.text}</span>;
+            case 'emoji':
+                return (
+                    <span role="img" aria-label={element.name} style={{ fontSize: '1.2em' }}>
+                        {element.unicode}
+                    </span>
+                );
+            case 'link':
+                return (
+                    <a href={element.url} style={element.style?.bold ? { fontWeight: 'bold' } : {}}>
+                        {element.text || element.url}
+                    </a>
+                );
+            case 'broadcast':
+                return (
+                    <span>
+                        <strong>@{element.range}</strong>
+                    </span>
+                );
+            default:
+                return <span>{element.text}</span>;
+        }
+    };
+
+    return (
+        <div>
+            {elements.map((element, index) => (
+                <React.Fragment key={index}>{renderElement(element)}</React.Fragment>
+            ))}
+        </div>
+    );
+};
+
+export default SlackMessage;
