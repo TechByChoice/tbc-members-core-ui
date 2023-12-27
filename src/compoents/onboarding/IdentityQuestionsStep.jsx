1ce1ae626d73021bcfb0605aa77156669e0a652f
Valerie Phoenix <valerie@techbychoice.org>
2023-12-27 10:26:36 -0800

    tbc-76 Updated the identity steps to include display question (#36)
    * Updated the identity steps to include display question
    
    * added logic to support profile updates with profile display details

--- src/compoents/onboarding/IdentityQuestionsStep.jsx
+++ src/compoents/onboarding/IdentityQuestionsStep.jsx
@@ -1,18 +1,15 @@
-import {
-    Typography,
-    FormControl,
-    FormLabel,
-    TextField,
-    Button,
-    Input, Grid, Autocomplete, Select, MenuItem, InputLabel
-} from '@mui/material';
-import React from "react";
-import {createFilterOptions} from "@mui/material/Autocomplete";
+import { Typography, FormControl, FormLabel, TextField, Button, Input, Grid, Autocomplete, Select, MenuItem, InputLabel } from '@mui/material';
+import React from 'react';
+import { createFilterOptions } from '@mui/material/Autocomplete';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const filter = createFilterOptions();
 
-function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, handleInputChange}) {
-    const filterOptions = (options, {inputValue}) => {
+function IdentityQuestionsStep({
+    answers, questions, handleAutocompleteChange, handleInputChange 
+}) {
+    const filterOptions = (options, { inputValue }) => {
         const matcher = new RegExp(inputValue, 'i');
         return options.filter(option => matcher.test(option));
     };
@@ -20,22 +17,18 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
         <Grid container spacing={3}>
             <Grid item xs={12}>
                 {/* Title */}
-                <Typography variant="h5">
-                    Identity Related Questions
-                </Typography>
+                <Typography variant="h5">Identity Related Questions</Typography>
                 <Typography variant="subtitle1">
-                    Thanks for all of that info! The next few questions will be about how you self identify. Please feel
-                    free to use the 'Other' option to type in any part of your identity that's not listed. This
-                    information will be used to help send you resources and events you may be interested in. We will not
-                    share the following information with anyone.
+                    Thanks for all of that info! The next few questions will be about how you self identify. Please feel free to use the &apos;Other&apos;
+                    option to type in any part of your identity that&apos;s not listed. This information will be used to help send you resources and events
+                    you may be interested in. We will not share the following information with anyone.
                 </Typography>
             </Grid>
 
             {/* Question 1 */}
             <Grid item xs={12}>
                 <FormControl variant="outlined" fullWidth>
-                    <FormLabel htmlFor="identity_sexuality">Please select the sexual identities that best describe you
-                        today.</FormLabel>
+                    <FormLabel htmlFor="identity_sexuality">Please select the sexual identities that best describe you today.</FormLabel>
                     <Autocomplete
                         multiple
                         selectOnFocus
@@ -46,7 +39,7 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                         isOptionEqualToValue={(option, value) =>
                             (option.inputValue && value.inputValue && option.inputValue === value.inputValue) || option === value
                         }
-                        getOptionLabel={(option) => {
+                        getOptionLabel={option => {
                             if (typeof option === 'string') {
                                 return option;
                             }
@@ -56,8 +49,8 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                         filterOptions={(options, params) => {
                             const filtered = filterOptions(options, params);
 
-                            const {inputValue} = params;
-                            const isExisting = options.some((option) => inputValue === option.name || option.identity);
+                            const { inputValue } = params;
+                            const isExisting = options.some(option => inputValue === option.name || option.identity);
                             if (inputValue !== '' && !isExisting) {
                                 filtered.push({
                                     inputValue,
@@ -68,22 +61,24 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                             return filtered;
                         }}
                         renderOption={(props, option) => <li {...props}>{option.name || option.identity}</li>}
-                        onChange={(event, value) => handleAutocompleteChange("identity_sexuality", value)}
-                        onChange={(event, value) => handleAutocompleteChange("identity_sexuality", value)}
-                        renderInput={(params) => <TextField name="identity_sexuality" {...params} />}
+                        onChange={(event, value) => handleAutocompleteChange('identity_sexuality', value)}
+                        renderInput={params => <TextField name="identity_sexuality" {...params} />}
                     />
                     <FormLabel>
-                        If you see any terms you're not sure about we encourage you to check out this site to learn more
-                        about people's experiences
+                        If you see any terms you&apos;re not sure about we encourage you to check out this site to learn more about people&apos;s
+                        experiences
                     </FormLabel>
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleInputChange} name="is_identity_sexuality_displayed" color="primary" size="small" />}
+                        label="Would you like to display your sexuality your profile?"
+                    />
                 </FormControl>
             </Grid>
 
             {/* Question 2 */}
             <Grid item xs={12}>
                 <FormControl variant="outlined" fullWidth>
-                    <FormLabel htmlFor="gender_identities">Please select the gender identities that best describe you
-                        today.</FormLabel>
+                    <FormLabel htmlFor="gender_identities">Please select the gender identities that best describe you today.</FormLabel>
                     <Autocomplete
                         multiple
                         selectOnFocus
@@ -95,7 +90,7 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                             (option.inputValue && value.inputValue && option.inputValue === value.inputValue) || option === value
                         }
                         name="gender_identities"
-                        getOptionLabel={(option) => {
+                        getOptionLabel={option => {
                             if (typeof option === 'string') {
                                 return option;
                             }
@@ -105,8 +100,8 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                         filterOptions={(options, params) => {
                             const filtered = filterOptions(options, params);
 
-                            const {inputValue} = params;
-                            const isExisting = options.some((option) => inputValue === option.name || option.gender);
+                            const { inputValue } = params;
+                            const isExisting = options.some(option => inputValue === option.name || option.gender);
                             if (inputValue !== '' && !isExisting) {
                                 filtered.push({
                                     inputValue,
@@ -117,8 +112,12 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                             return filtered;
                         }}
                         renderOption={(props, option) => <li {...props}>{option.name || option.gender}</li>}
-                        onChange={(event, value) => handleAutocompleteChange("gender_identities", value)}
-                        renderInput={(params) => <TextField name="gender_identities" {...params} />}
+                        onChange={(event, value) => handleAutocompleteChange('gender_identities', value)}
+                        renderInput={params => <TextField name="gender_identities" {...params} />}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleInputChange} name="is_identity_gender_displayed" color="primary" size="small" />}
+                        label="Would you like to display your gender your profile?"
                     />
                 </FormControl>
             </Grid>
@@ -126,8 +125,7 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
             {/* Question 3 */}
             <Grid item xs={12}>
                 <FormControl variant="outlined" fullWidth>
-                    <FormLabel htmlFor="identity_ethic">Please select all the identities that best describe your ethic
-                        background</FormLabel>
+                    <FormLabel htmlFor="identity_ethic">Please select all the identities that best describe your ethic background</FormLabel>
                     <Autocomplete
                         multiple
                         selectOnFocus
@@ -138,7 +136,7 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                         isOptionEqualToValue={(option, value) =>
                             (option.inputValue && value.inputValue && option.inputValue === value.inputValue) || option === value
                         }
-                        getOptionLabel={(option) => {
+                        getOptionLabel={option => {
                             if (typeof option === 'string') {
                                 return option;
                             }
@@ -148,8 +146,8 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                         filterOptions={(options, params) => {
                             const filtered = filterOptions(options, params);
 
-                            const {inputValue} = params;
-                            const isExisting = options.some((option) => inputValue === option.name || option.ethnicity);
+                            const { inputValue } = params;
+                            const isExisting = options.some(option => inputValue === option.name || option.ethnicity);
                             if (inputValue !== '' && !isExisting) {
                                 filtered.push({
                                     inputValue,
@@ -160,8 +158,12 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                             return filtered;
                         }}
                         renderOption={(props, option) => <li {...props}>{option.name || option.ethnicity}</li>}
-                        onChange={(event, value) => handleAutocompleteChange("identity_ethic", value)}
-                        renderInput={(params) => <TextField name="ethic_identities" {...params} />}
+                        onChange={(event, value) => handleAutocompleteChange('identity_ethic', value)}
+                        renderInput={params => <TextField name="ethic_identities" {...params} />}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleInputChange} name="is_identity_ethic_displayed" color="primary" size="small" />}
+                        label="Would you like to display your ethnicity your profile?"
                     />
                 </FormControl>
             </Grid>
@@ -169,13 +171,18 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
             {/* Question 4 */}
             <Grid item xs={12}>
                 <FormControl variant="outlined" fullWidth>
-                    <FormLabel htmlFor="disability">Have you ever identified yourself as someone with a disability?
-                        Both visible and non-visible disabilities included.</FormLabel>
+                    <FormLabel htmlFor="disability">
+                        Have you ever identified yourself as someone with a disability? Both visible and non-visible disabilities included.
+                    </FormLabel>
                     <Select id="disability" name="disability">
                         <MenuItem value={true}>Yes</MenuItem>
                         <MenuItem value={false}>No</MenuItem>
                         <MenuItem value="Prefer not to answer">Prefer not to answer</MenuItem>
                     </Select>
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleInputChange} name="is_disability_displayed" color="primary" size="small" />}
+                        label="Would you like to display your disability status your profile?"
+                    />
                 </FormControl>
             </Grid>
 
@@ -188,6 +195,10 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                         <MenuItem value={false}>No</MenuItem>
                         <MenuItem value="Prefer not to answer">Prefer not to answer</MenuItem>
                     </Select>
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleInputChange} name="is_care_giver_displayed" color="primary" size="small" />}
+                        label="Would you like to display your care giver status your profile?"
+                    />
                 </FormControl>
             </Grid>
 
@@ -196,15 +207,18 @@ function IdentityQuestionsStep({answers, questions, handleAutocompleteChange, ha
                 <FormControl variant="outlined" fullWidth>
                     <FormLabel htmlFor="veteran_status">What is your Veteran Status?</FormLabel>
                     <Select onChange={handleInputChange} name="veteran_status" id="veteran_status">
-                        <MenuItem value='1'>Yes</MenuItem>
-                        <MenuItem value='2'>No</MenuItem>
+                        <MenuItem value="1">Yes</MenuItem>
+                        <MenuItem value="2">No</MenuItem>
                         <MenuItem value="3">Prefer not to answer</MenuItem>
                     </Select>
+                    <FormControlLabel
+                        control={<Checkbox onChange={handleInputChange} name="is_veteran_status_displayed" color="primary" size="small" />}
+                        label="Would you like to display your veteran status your profile?"
+                    />
                 </FormControl>
             </Grid>
         </Grid>
     );
 }
-
 
 export default IdentityQuestionsStep;
