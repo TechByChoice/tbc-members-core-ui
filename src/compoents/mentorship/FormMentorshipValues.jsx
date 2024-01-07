bff31a6e9e82b8f009d00cef6ba2c9f510fafb86
Patrick Organ <patrick@permafrost.dev>
2024-01-07 09:15:47 -0500

    refactor mentorship values form

--- src/compoents/mentorship/FormMentorshipValues.jsx
+++ src/compoents/mentorship/FormMentorshipValues.jsx
@@ -1,21 +1,44 @@
-import {
-    Autocomplete,
-    Button, Checkbox,
-    FormControl, FormControlLabel,
-    FormHelperText,
-    FormLabel,
-    Grid,
-    OutlinedInput,
-    TextField,
-    Typography
-} from "@mui/material";
-import React, {useEffect, useState} from "react";
-import {createFilterOptions} from "@mui/material/Autocomplete";
-import Slider from "@mui/material/Slider";
+import { FormControl, FormLabel, Grid, Typography } from '@mui/material';
+import Slider from '@mui/material/Slider';
+import React, { useState } from 'react';
 
+function valuetext(value) {
+    return `${value}`;
+}
 
-export default function FormMentorshipValues({onFormDataChange}) {
-    const [values, setValues] = useState({
+const marks = [
+    { value: 0, label: '0' },
+    { value: 1, label: '1' },
+    { value: 2, label: '2' },
+    { value: 3, label: '3' },
+    { value: 4, label: '4' },
+    { value: 5, label: '5' },
+    { value: 6, label: '6' },
+    { value: 7, label: '7' },
+    { value: 8, label: '8' },
+];
+
+const ValueItem = ({ description, name, onChange }) => (
+    <Grid item xs={12}>
+        <FormControl>
+            <FormLabel>{description}</FormLabel>
+            <Slider
+                aria-label={`${name} value`}
+                defaultValue={0}
+                name={name}
+                getAriaValueText={valuetext}
+                min={0}
+                max={8}
+                valueLabelDisplay="auto"
+                marks={marks}
+                onChange={(e, newValue) => onChange(name, newValue)}
+            />
+        </FormControl>
+    </Grid>
+);
+
+export default function FormMentorshipValues({ onFormDataChange }) {
+    const [ values, setValues ] = useState({
         power: 0,
         achievement: 0,
         hedonism: 0,
@@ -27,251 +50,78 @@ export default function FormMentorshipValues({onFormDataChange}) {
         conformity: 0,
         security: 0,
     });
-    const marks = [
-        {value: 0, label: "0"},
-        {value: 1, label: "1"},
-        {value: 2, label: "2"},
-        {value: 3, label: "3"},
-        {value: 4, label: "4"},
-        {value: 5, label: "5"},
-        {value: 6, label: "6"},
-        {value: 7, label: "7"},
-        {value: 8, label: "8"}
-    ]
 
-    function valuetext(value) {
-        return `${value}`;
-    }
+    const handleChange = (name, newValue) => {
+        const updatedValues = { ...values, [name]: newValue };
 
-    const handelChange = (name, newValue) => {
-        const updatedValues = {...values, [name]: newValue};
         setValues(updatedValues);
         onFormDataChange(updatedValues);
-    }
+    };
 
     return (
         <>
             <Grid container>
                 <Grid item xs={12}>
                     <Typography variant="h6">Values Based Questions</Typography>
-                    <hr/>
+                    <hr />
                 </Grid>
                 <Grid item xs={12} md={4} spacing={3} mt={3}>
                     <Typography variant="body">
-                        We want to match you with a mentee who shares your values and beliefs, so we would love to learn
-                        more about you! Please take a moment to answer the following questions regarding your personal
-                        values.
+                        We want to match you with a mentee who shares your values and beliefs, so we would love to learn more about you! Please take a
+                        moment to answer the following questions regarding your personal values.
                     </Typography>
                 </Grid>
                 <Grid item xs={8}>
                     <Grid container>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    POWER (social power, authority, wealth)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="Power value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="power"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('power', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    ACHIEVEMENT (success, capability, ambition, influence on people and events)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="Achievement value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="achievement"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('achievement', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    HEDONISM (gratification of desires, enjoyment in life, self-indulgence)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="hedonism value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="hedonism"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('hedonism', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    STIMULATION (daring, a varied and challenging life, an exciting life)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="stimulation value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="stimulation"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('stimulation', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    SELF DIRECTION (creativity, freedom, curiosity, independence, choosing one’s own
-                                    goals)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="self direction value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="self_direction"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('self_direction', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    Universalism (broadmindedness, beauty of nature and art., social justice, a world at
-                                    peace, quality, wisdom, unity with nature, environmental protection)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="universalism value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="universalism"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('universalism', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    BENEVOLENCE (helpfulness, honesty, forgiveness, loyalty, responsibility)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="benevolence value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="benevolence"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('benevolence', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    TRADITION (respect for tradition, humbleness, accepting one's portion in life,
-                                    devotion, modesty)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="tradition value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="tradition"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('tradition', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    CONFORMITY (obedience, honoring parents and elders, self-discipline, politeness)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="conformity value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="conformity"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('conformity', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <FormControl>
-                                <FormLabel>
-                                    SECURITY (national security, family security, social order, cleanliness,
-                                    reciprocation of favors)
-                                </FormLabel>
-                                <Slider
-                                    aria-label="security value"
-                                    defaultValue={0}
-                                    // valueLabelFormat={valueLabelFormat}
-                                    name="security"
-                                    getAriaValueText={valuetext}
-                                    min={0}
-                                    max={8}
-                                    valueLabelDisplay="auto"
-                                    marks={marks}
-                                    onChange={(e, newValue) => handelChange('security', newValue)}
-                                />
-                            </FormControl>
-                        </Grid>
+                        <ValueItem description="POWER (social power, authority, wealth)" name="power" onChange={handleChange} />
+                        <ValueItem
+                            description="ACHIEVEMENT (success, capability, ambition, influence on people and events)"
+                            name="achievement"
+                            onChange={handleChange}
+                        />
+                        <ValueItem
+                            description="HEDONISM (gratification of desires, enjoyment in life, self-indulgence)"
+                            name="hedonism"
+                            onChange={handleChange}
+                        />
+                        <ValueItem
+                            description="STIMULATION (daring, a varied and challenging life, an exciting life)"
+                            name="stimulation"
+                            onChange={handleChange}
+                        />
+                        <ValueItem
+                            description="SELF DIRECTION (creativity, freedom, curiosity, independence, choosing one’s own goals)"
+                            name="selfDirection"
+                            onChange={handleChange}
+                        />
+                        <ValueItem
+                            description="UNIVERSALISM (broadmindedness, beauty of nature and art., social justice, a world at peace, quality, wisdom, unity with nature, environmental protection)"
+                            name="universalism"
+                            onChange={handleChange}
+                        />
+                        <ValueItem
+                            description="BENEVOLENCE (helpfulness, honesty, forgiveness, loyalty, responsibility)"
+                            name="benevolence"
+                            onChange={handleChange}
+                        />
+                        <ValueItem
+                            description="TRADITION (respect for tradition, humbleness, accepting one's portion in life, devotion, modesty)"
+                            name="tradition"
+                            onChange={handleChange}
+                        />
+                        <ValueItem
+                            description="CONFORMITY (obedience, honoring parents and elders, self-discipline, politeness)"
+                            name="conformity"
+                            onChange={handleChange}
+                        />
+                        <ValueItem
+                            description="SECURITY (national security, family security, social order, cleanliness, reciprocation of favors)"
+                            name="security"
+                            onChange={handleChange}
+                        />
                     </Grid>
                 </Grid>
             </Grid>
         </>
-    )
+    );
 }
