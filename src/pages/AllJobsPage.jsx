eb481cbefddfc3e0233a9490d48897a3216e8bb2
Valerie Phoenix <valerie@techbychoice.org>
2024-01-18 09:22:31 -0800

    tbc-104 hide "jobs you posted" box if user did not post any jobs (#67)
    - Added check to hide "Your Posted Jobs" card if user hasn't posted a job

--- src/pages/AllJobsPage.jsx
+++ src/pages/AllJobsPage.jsx
@@ -3,6 +3,7 @@ import React, { Component, useEffect, useState } from 'react';
 import { Button, Grid, Typography } from '@mui/material';
 import JobCard from '../compoents/JobCard';
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 const CalloutCard = styled.section`
     border: 1px solid darkgray;
@@ -60,35 +61,44 @@ export default function AllJobsPage({}) {
 
     return (
         <JobWrapper>
-            <CalloutCard>
-                <Typography variant="h5" mb={1}>
-                    Jobs You Posted
-                </Typography>
-                <Button variant="contained" color="primary" onClick={pullJobs}>
-                    Pull Remote Jobs
-                </Button>
-                <Grid container spacing={4}>
-                    {userPostedJobs ? (
-                        userPostedJobs.map((job, index) => (
-                            <Grid item xs={12} sm={6} md={4} key={index}>
-                                <JobCard
-                                    match={false}
-                                    companyLogo={job?.parent_company?.logo}
-                                    companyName={job?.parent_company?.name}
-                                    jobType={job?.role?.name}
-                                    jobTitle={job?.job_title}
-                                    jobId={job?.id}
-                                    location={job?.location}
-                                    salary={`${job?.max_compensation?.range} - ${job?.max_compensation?.range}`}
-                                    description={null}
-                                />
-                            </Grid>
-                        ))
-                    ) : (
-                        <p>Loading events...</p> // Or any other loading indicator
-                    )}
-                </Grid>
-            </CalloutCard>
+            {userPostedJobs.length > 0 ? (
+                <CalloutCard>
+                    <Typography variant="h5" mb={1}>
+                        Jobs You Posted
+                    </Typography>
+                    <Grid container spacing={4}>
+                        {userPostedJobs ? (
+                            userPostedJobs.map((job, index) => (
+                                <Grid item xs={12} sm={6} md={4} key={index}>
+                                    <JobCard
+                                        match={false}
+                                        companyLogo={job?.parent_company?.logo}
+                                        companyName={job?.parent_company?.name}
+                                        jobType={job?.role?.name}
+                                        jobTitle={job?.job_title}
+                                        jobId={job?.id}
+                                        location={job?.location}
+                                        salary={`${job?.max_compensation?.range} - ${job?.max_compensation?.range}`}
+                                        description={null}
+                                    />
+                                </Grid>
+                            ))
+                        ) : (
+                            <p>Loading events...</p> // Or any other loading indicator
+                        )}
+                    </Grid>
+                </CalloutCard>
+            ) : (
+                <CalloutCard>
+                    <Typography component="h5" variant="h5">
+                        Would you like to post a job?
+                    </Typography>
+                    <Link to="/job/new/referral">
+                        <Button variant="contained">Add Job</Button>
+                    </Link>
+                </CalloutCard>
+            )}
+
             <Grid container spacing={4}>
                 {jobs ? (
                     jobs.map((job, index) => (
