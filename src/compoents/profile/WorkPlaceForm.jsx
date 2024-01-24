005dc84ff097e45904ac22a613e35ee8143c5e5f
DeltaV93 <valerie@techbychoice.org>
2024-01-23 22:45:37 -0800

    updates to fix build

--- src/compoents/profile/WorkPlaceForm.jsx
+++ src/compoents/profile/WorkPlaceForm.jsx
@@ -1,33 +1,27 @@
-import {
-    Autocomplete,
-    Button,
-    FormControl,
-    FormHelperText,
-    FormLabel,
-    Grid,
-    InputAdornment,
-    OutlinedInput, TextField,
-    Typography
-} from "@mui/material";
-import React, {useEffect, useState} from "react";
-import {useAuth} from "../../providers/AuthProvider";
-import {createFilterOptions} from "@mui/material/Autocomplete";
-import {useStatus} from "../../providers/MsgStatusProvider";
+import { Autocomplete, Button, FormControl, FormHelperText, FormLabel, Grid, InputAdornment, OutlinedInput, TextField, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../providers/AuthProvider';
+import { createFilterOptions } from '@mui/material/Autocomplete';
+import { useStatus } from '../../providers/MsgStatusProvider';
+import { routes } from '../../lib/routes';
+import CompanyDropdownUpdate from '../DropdownCompany';
 
 const filter = createFilterOptions();
-export default function WorkPlaceForm({questions}) {
-    const {user} = useAuth();
-    const {viewNewCompany, setViewNewCompany} = useState()
-    const userDetails = user[0]
-    const [formErrors, setFormErrors] = useState({});
-    const {statusType, setStatusMessage, setIsAlertOpen, setStatusType} = useStatus();
+export default function WorkPlaceForm({ questions }) {
+    const { user } = useAuth();
+    const { viewNewCompany, setViewNewCompany } = useState();
+    const userDetails = user[0];
+    const [ formErrors, setFormErrors ] = useState({});
+    const {
+        statusType, setStatusMessage, setIsAlertOpen, setStatusType 
+    } = useStatus();
 
-    const [formData, setFormData] = useState({
+    const [ formData, setFormData ] = useState({
         company_name: '',
         company_url: '',
         job_roles: [],
         company_id: '',
-        company: []
+        company: [],
     });
     const extractDefaultValues = () => {
         // Extract the skills and roles from userDetails and map them to the corresponding objects in questions.
@@ -39,7 +33,7 @@ export default function WorkPlaceForm({questions}) {
             company_url: '',
             job_roles: [],
             company_id: '',
-            company: []
+            company: [],
         };
 
         if (userDetails && userDetails?.user_info?.talentprofile?.role) {
@@ -56,7 +50,6 @@ export default function WorkPlaceForm({questions}) {
                 //     defaults.job_roles.push(identityItem);
                 // }
                 // defaults.job_roles.push(role.name);
-
             });
         }
 
@@ -76,18 +69,18 @@ export default function WorkPlaceForm({questions}) {
         return defaults;
     };
     useEffect(() => {
-        const defaultValues = extractDefaultValues()
+        const defaultValues = extractDefaultValues();
         if (userDetails) {
             setFormData({
                 company_name: userDetails?.user_info?.userprofile.company_name || '',
                 job_roles: defaultValues.job_roles,
-                company: defaultValues.company
+                company: defaultValues.company,
             });
         }
-    }, [userDetails]);
+    }, [ userDetails ]);
 
-    const handelAccountDetails = (e) => {
-        e.preventDefault()
+    const handelAccountDetails = e => {
+        e.preventDefault();
 
         const url = process.env.REACT_APP_API_BASE_URL + 'user/profile/update/work-place';
         fetch(url, {
@@ -95,10 +88,10 @@ export default function WorkPlaceForm({questions}) {
             credentials: 'include',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Token ${localStorage.getItem('token')}`
+                Authorization: `Token ${localStorage.getItem('token')}`,
                 // 'credentials': 'include',
             },
-            body: JSON.stringify(formData)
+            body: JSON.stringify(formData),
         })
             .then(response => response.json())
             .then(data => {
@@ -108,19 +101,18 @@ export default function WorkPlaceForm({questions}) {
                     setStatusMessage('Updates have been saved');
                 }
             })
-            .catch((error) => {
-
+            .catch(error => {
                 setIsAlertOpen(true);
                 setStatusType('error');
                 setStatusMessage('error');
             });
     };
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
+    const handleChange = e => {
+        const { name, value } = e.target;
 
         let updatedValue = value;
-        if (name === "company_url" && !value.startsWith("https://")) {
+        if (name === 'company_url' && !value.startsWith('https://')) {
             updatedValue = `https://${value}`;
         }
 
@@ -131,18 +123,12 @@ export default function WorkPlaceForm({questions}) {
     };
 
     const handleAutocompleteChange = (name, value) => {
-
         // Check if the value is an array (since Autocomplete can be multiple)
         if (Array.isArray(value)) {
-            value = value.map(item =>
-                item.name || item
-            );
-
-        } else {
-            value = item.inputValue || item[name] || item;
-
+            value = value.map(item => item.name || item);
         }
-        setFormData(prev => ({...prev, [name]: value}));
+
+        setFormData(prev => ({ ...prev, [name]: value }));
     };
 
     return (
@@ -150,7 +136,7 @@ export default function WorkPlaceForm({questions}) {
             <Grid container>
                 <Grid item xs={12}>
                     <Typography variant="h6">Work Place Details</Typography>
-                    <hr/>
+                    <hr />
                 </Grid>
                 <Grid item xs={12} md={4} spacing={3} mt={3}>
                     <Typography variant="body">Update details about your job & title</Typography>
@@ -162,19 +148,18 @@ export default function WorkPlaceForm({questions}) {
                                 <Grid item xs={12}>
                                     <FormControl fullWidth>
                                         <FormLabel htmlFor="company_name">Company Name</FormLabel>
-                                        <OutlinedInput onChange={handleChange}
-                                                       name="company_name"
-                                                       type="text"/>
+                                        <OutlinedInput onChange={handleChange} name="company_name" type="text" />
                                     </FormControl>
                                 </Grid>
                                 <Grid item xs={12}>
                                     <FormControl fullWidth>
                                         <FormLabel htmlFor="company_url">Company Website</FormLabel>
-                                        <OutlinedInput onChange={handleChange}
-                                                       name="company_url"
-                                                       startAdornment={<InputAdornment
-                                                           position="start">https://</InputAdornment>}
-                                                       type="text"/>
+                                        <OutlinedInput
+                                            onChange={handleChange}
+                                            name="company_url"
+                                            startAdornment={<InputAdornment position="start">https://</InputAdornment>}
+                                            type="text"
+                                        />
                                     </FormControl>
                                 </Grid>
                             </>
@@ -182,47 +167,12 @@ export default function WorkPlaceForm({questions}) {
                             <Grid item xs={12}>
                                 <FormControl fullWidth>
                                     <FormLabel id="company-label">Please select the company you work with.</FormLabel>
-                                    <Autocomplete
-                                        multiple
-                                        selectOnFocus
-                                        value={formData.company}
-                                        includeInputInList
-                                        handleHomeEndKeys
-                                        id='company_list'
-                                        aria-labelledby="company-label"
-                                        options={questions?.company_list || []} // <-- directly provide a default value here
-                                        isOptionEqualToValue={(option, value) =>
-                                            (option.inputValue) || option === value
-                                        }
-                                        getOptionLabel={(option) => {
-                                            if (typeof option === 'string') {
-                                                return option;
-                                            }
-                                            // Check for the special case where the option has an inputValue property
-                                            if (option.inputValue) return option.inputValue;
-
-                                            // Existing logic
-                                            return option.company_name
-                                        }}
-                                        filterOptions={(options, params) => {
-                                            const filtered = filter(options, params);
-
-                                            const {inputValue} = params;
-                                            // Suggest the creation of a new value
-                                            const isExisting = options.some((option) => inputValue === option.company_name);
-                                            if (inputValue !== '' && !isExisting) {
-                                                filtered.push({
-                                                    inputValue,
-                                                    name: `Add "${inputValue}"`,
-                                                });
-                                            }
-
-                                            return filtered;
-                                        }}
-                                        renderOption={(props, option) =>
-                                            <li {...props}>{option.company_name || option.name}</li>}
-                                        onChange={(event, value) => handleAutocompleteChange("company_id", value)}
-                                        renderInput={(params) => <TextField name="company_id" {...params} />}
+                                    <CompanyDropdownUpdate
+                                        error={formErrors}
+                                        answers={formData}
+                                        setAnswers={setFormData}
+                                        isRequired={false}
+                                        onCompanySelect={handleAutocompleteChange}
                                     />
                                 </FormControl>
                             </Grid>
@@ -230,9 +180,7 @@ export default function WorkPlaceForm({questions}) {
                         {/* Job Title Dropdown */}
                         <Grid item xs={12}>
                             <FormControl fullWidth error={!!formErrors.job_roles}>
-                                <FormLabel id="job-title-label">What is the job title that best fits your desired or
-                                    current
-                                    position?</FormLabel>
+                                <FormLabel id="job-title-label">What is the job title that best fits your desired or current position?</FormLabel>
                                 <Autocomplete
                                     multiple
                                     value={formData.job_roles}
@@ -240,13 +188,13 @@ export default function WorkPlaceForm({questions}) {
                                     selectOnFocus
                                     includeInputInList
                                     handleHomeEndKeys
-                                    id='job_roles'
+                                    id="job_roles"
                                     aria-labelledby="job-title-label"
                                     options={questions?.job_roles || []} // <-- directly provide a default value here
                                     isOptionEqualToValue={(option, value) =>
                                         (option.inputValue && value.inputValue && option.inputValue === value.inputValue) || option === value
                                     }
-                                    getOptionLabel={(option) => {
+                                    getOptionLabel={option => {
                                         if (typeof option === 'string') {
                                             return option;
                                         }
@@ -254,14 +202,14 @@ export default function WorkPlaceForm({questions}) {
                                         if (option.inputValue) return option.inputValue;
 
                                         // Existing logic
-                                        return option.name
+                                        return option.name;
                                     }}
                                     filterOptions={(options, params) => {
                                         const filtered = filter(options, params);
 
-                                        const {inputValue} = params;
+                                        const { inputValue } = params;
                                         // Suggest the creation of a new value
-                                        const isExisting = options.some((option) => inputValue === option.name);
+                                        const isExisting = options.some(option => inputValue === option.name);
                                         if (inputValue !== '' && !isExisting) {
                                             filtered.push({
                                                 inputValue,
@@ -271,18 +219,15 @@ export default function WorkPlaceForm({questions}) {
 
                                         return filtered;
                                     }}
-                                    renderOption={(props, option) =>
-                                        <li {...props}>{option.pronouns || option.name}</li>}
-                                    onChange={(event, value) => handleAutocompleteChange("job_roles", value)}
-                                    renderInput={(params) => <TextField name="job-title-label" {...params} />}
+                                    renderOption={(props, option) => <li {...props}>{option.pronouns || option.name}</li>}
+                                    onChange={(event, value) => handleAutocompleteChange('job_roles', value)}
+                                    renderInput={params => <TextField name="job-title-label" {...params} />}
                                 />
                                 {!!formErrors.job_roles && <FormHelperText>{formErrors.job_roles}</FormHelperText>}
                             </FormControl>
                         </Grid>
                         <Grid item xs={12} sm={8}>
-                            <Button variant="contained"
-                                    color="primary"
-                                    type="submit">
+                            <Button variant="contained" color="primary" type="submit">
                                 Save
                             </Button>
                         </Grid>
@@ -290,6 +235,5 @@ export default function WorkPlaceForm({questions}) {
                 </Grid>
             </Grid>
         </form>
-    )
+    );
 }
-
