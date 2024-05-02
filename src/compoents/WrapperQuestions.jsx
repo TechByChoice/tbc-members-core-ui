f8f63e2fe79d762f92d6f9a2cafb561d40a4ce1a
DeltaV93 <valerie@techbychoice.org>
2024-05-02 11:41:49 -0700

    added alerts

--- src/compoents/WrapperQuestions.jsx
+++ src/compoents/WrapperQuestions.jsx
@@ -1,15 +1,19 @@
 import React, { Suspense } from 'react';
 import { useAuth } from '@/providers/AuthProvider';
+import { useStatusMessage } from '@/hooks/useStatusMessage';
+import { useNavigate } from 'react-router-dom';
 
 const SurveyQuestions = React.lazy(() => import('open_doors/SurveyQuestions'));
 export default function WrapperQuestions() {
     const { user } = useAuth();
     const userId = user?.[0]?.user_info?.id;
+    const statusMessage = useStatusMessage();
+    const history = useNavigate();
 
     return (
         <>
             <Suspense fallback={<div>Loading...</div>}>
-                <SurveyQuestions userId={userId} />
+                <SurveyQuestions history={history} statusMessage={statusMessage} userId={userId} />
             </Suspense>
         </>
     );
