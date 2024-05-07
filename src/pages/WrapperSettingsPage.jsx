012a7681dcd97fa460a2a6b68114bb62f3aa4501
DeltaV93 <valerie@techbychoice.org>
2024-05-07 15:21:37 -0700

    fixed typo

--- src/pages/WrapperSettingsPage.jsx
+++ src/pages/WrapperSettingsPage.jsx
@@ -6,12 +6,17 @@ import OpenDoorsSettingPage from '@/pages/OpenDoorsSettingPage';
 
 const WrapperSettingsPage = () => {
     const { user } = useAuth();
+    const accountInfo = user[0]?.account_info;
+
+    if (!accountInfo) {
+        return <p>Loading settings...</p>;
+    }
 
     return (
         <>
-            {user[0]?.account_info?.is_member && <ProfileSettingPage />}
-            {user[0]?.account_info?.is_company_account && <CompanySettingPage />}
-            {user[0]?.account_info?.is_open_doors && <OpenDoorsSettingPage />}
+            {accountInfo.is_member && <ProfileSettingPage />}
+            {accountInfo.is_company_account && <CompanySettingPage />}
+            {accountInfo.is_open_doors && <OpenDoorsSettingPage />}
         </>
     );
 };
