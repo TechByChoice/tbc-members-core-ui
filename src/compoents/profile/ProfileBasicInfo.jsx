92d493a34c8a1b13e26be52f012721d9e3428d11
DeltaV93 <valerie@techbychoice.org>
2024-05-19 07:54:44 -0700

    only show workplace and social mediia form if user is not a company account

--- src/compoents/profile/ProfileBasicInfo.jsx
+++ src/compoents/profile/ProfileBasicInfo.jsx
@@ -1,32 +1,37 @@
-import {Button, FormControl, FormLabel, Grid, InputAdornment, OutlinedInput, Typography} from "@mui/material";
-import React from "react";
-import {useAuth} from "../../providers/AuthProvider";
-import {useStatus} from "../../providers/MsgStatusProvider";
-import AccountDetailsForm from "./AccountDetailsForm";
-import WorkPlaceForm from "./WorkPlaceForm";
-import SocialMediaForm from "./SocialMediaForm";
+import { Button, FormControl, FormLabel, Grid, InputAdornment, OutlinedInput, Typography } from '@mui/material';
+import React from 'react';
+import { useAuth } from '../../providers/AuthProvider';
+import { useStatus } from '../../providers/MsgStatusProvider';
+import AccountDetailsForm from './AccountDetailsForm';
+import WorkPlaceForm from './WorkPlaceForm';
+import SocialMediaForm from './SocialMediaForm';
 
-
-export default function ProfileBasicInfo({handleChange, questions, formErrors}) {
-    const {user} = useAuth();
-    const userDetails = user[0]
-    const {statusType, setStatusMessage, setIsAlertOpen, setStatusType} = useStatus();
-
+export default function ProfileBasicInfo({ handleChange, questions, formErrors }) {
+    const { user } = useAuth();
+    const userDetails = user[0];
+    const {
+        statusType, setStatusMessage, setIsAlertOpen, setStatusType 
+    } = useStatus();
 
     function handleSave() {
-        console.log('saved')
+        console.log('saved');
     }
 
-
-    return (<Grid container>
-        <>
-            <AccountDetailsForm/>
-        </>
-        <>
-            <WorkPlaceForm questions={questions}/>
-        </>
-        <>
-            <SocialMediaForm />
-        </>
-    </Grid>)
+    return (
+        <Grid container>
+            <>
+                <AccountDetailsForm />
+            </>
+            {!userDetails?.is_company_account && (
+                <>
+                    <>
+                        <WorkPlaceForm questions={questions} />
+                    </>
+                    <>
+                        <SocialMediaForm />
+                    </>
+                </>
+            )}
+        </Grid>
+    );
 }
