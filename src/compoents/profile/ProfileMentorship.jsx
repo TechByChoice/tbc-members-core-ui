7b636fd9c94af60ca360616c1e0dbf4596f89138
DeltaV93 <valerie@techbychoice.org>
2024-07-05 21:32:39 -0700

    fixed mentorship page to only show editable content if the application is completed

--- src/compoents/profile/ProfileMentorship.jsx
+++ src/compoents/profile/ProfileMentorship.jsx
@@ -3,20 +3,26 @@ import React, { useEffect, useState } from 'react';
 import { createFilterOptions } from '@mui/material/Autocomplete';
 import FormMentorApplication from '../mentorship/FormMentorApplication';
 import ProfileMentorDetails from './ProfileMentorDetails';
-import { useAuth } from '../../providers/AuthProvider';
+import { useAuth } from '@/providers/AuthProvider';
 import ProfileCalLinkForm from './ProfileCalLinkForm';
 import Button from '@mui/material/Button';
-import { useStatus } from '../../providers/MsgStatusProvider';
+import { useStatus } from '@/providers/MsgStatusProvider';
 import { routes } from '@/lib/routes';
 import { useStatusMessage } from '@/hooks/useStatusMessage';
+import { FeatureCard } from '@/compoents/FeatuerCards';
 
 const filter = createFilterOptions();
 
 export default function ProfileMentorship({ questions }) {
     const { user } = useAuth();
-    const statusMessage = useStatusMessage();
-
     let userData = user[0];
+    const statusMessage = useStatusMessage();
+    const accountInfo = user[0]?.account_info || {};
+    const mentorDetails = user[0]?.mentor_details || {};
+
+    const isMentorshipProgram = accountInfo.is_mentee || accountInfo.is_mentor;
+    const isNeedsToSubmitMentorshipApplication = !accountInfo.is_mentor_application_submitted && isMentorshipProgram;
+
     const [ formData, setFormData ] = useState({
         commitment_level: [],
         mentee_support_areas: [],
@@ -64,30 +70,45 @@ export default function ProfileMentorship({ questions }) {
                 console.error('Error:', error);
             });
     };
+
+    const NonMentorContent = () => (
+        <>
+            <Typography variant="h6">We&apos;re currently not taking applications for our mentorship program.</Typography>
+            <Typography variant="body1">We&apos;re currently at our reviewing capacity. Once we address these we will open the application back up.</Typography>
+        </>
+    );
+
     return (
         <>
             <Grid container>
-                {user[0]?.account_info?.is_mentor ? (
+                {isMentorshipProgram ? (
                     <>
-                        <FormMentorApplication defaultValues={user[0]?.mentor_details} formData={formData} setFormData={setFormData} onFormDataChange={undefined} />
-                        <Button variant="contained" color="primary" onClick={handelFormSubmit}>
-                            Save
-                        </Button>
-                        <ProfileMentorDetails defaultValues={user[0]?.mentor_details} />
-                        {!user[0]?.account_info?.is_mentor_profile_active ||
-                            (user[0]?.account_info?.is_mentor_profile_approved && user[0].mentor_details && (
-                                <>
-                                    <ProfileCalLinkForm />
-                                </>
-                            ))}
+                        {!isNeedsToSubmitMentorshipApplication ? (
+                            <>
+                                <FormMentorApplication defaultValues={mentorDetails} formData={formData} setFormData={setFormData} onFormDataChange={undefined} />
+                                <Button variant="contained" color="primary" onClick={handelFormSubmit}>
+                                    Save
+                                </Button>
+                                <ProfileMentorDetails defaultValues={mentorDetails} />
+                                {!accountInfo.is_mentor_profile_active || (accountInfo.is_mentor_profile_approved && mentorDetails && <ProfileCalLinkForm />)}
+                            </>
+                        ) : (
+                            <>
+                                <h1>Finish your mentorship program application</h1>
+                                <Grid item xs={12} sm={6}>
+                                    <FeatureCard
+                                        image="https://uploads-ssl.webflow.com/5fc123904bcd576087dd38e2/6671cc4505016af9986993cc_Paperwork%20Icon.svg"
+                                        subTitle="Mentorship Program"
+                                        linkEndpoint="/mentor/create"
+                                        btnText="Apply"
+                                        title="Apply to Today"
+                                    />
+                                </Grid>
+                            </>
+                        )}
                     </>
                 ) : (
-                    <>
-                        <Typography variant="h6">We&apos;re currently not taking applications for our mentorship program.</Typography>
-                        <Typography variant="body1">
-                            We&apos;re currently at our reviewing capacity. Once we address these we will open the application back up.
-                        </Typography>
-                    </>
+                    <NonMentorContent />
                 )}
             </Grid>
         </>
