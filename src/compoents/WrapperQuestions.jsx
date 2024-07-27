580c73378674b6e1221729c723df4a1a340d567c
DeltaV93 <valerie@techbychoice.org>
2024-07-27 10:50:24 -0700

    Added checking for user id on page load

--- src/compoents/WrapperQuestions.jsx
+++ src/compoents/WrapperQuestions.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { useAuth } from '@/providers/AuthProvider';
 import { useStatusMessage } from '@/hooks/useStatusMessage';
 import { useNavigate } from 'react-router-dom';
@@ -6,9 +6,20 @@ import { useNavigate } from 'react-router-dom';
 const SurveyQuestions = React.lazy(() => import('open_doors/SurveyQuestions'));
 export default function WrapperQuestions() {
     const { user } = useAuth();
-    const userId = user?.[0]?.user_info?.id;
+    const [ userId, setUserId ] = useState(null);
+    // const userId = user?.[0]?.user_info?.id;
     const statusMessage = useStatusMessage();
     const history = useNavigate();
+
+    useEffect(() => {
+        if (user && user[0] && user[0].user_info) {
+            setUserId(user[0].user_info.id);
+        }
+    }, [ user ]);
+
+    if (!userId) {
+        return <div>Loading user data...</div>;
+    }
 
     return (
         <>
